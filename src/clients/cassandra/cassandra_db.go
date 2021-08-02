801e587ef78f7473f4a01439e773435f4c7e0a87
Ayush Paudel <ayushpaudel20@gmail.com>
2021-08-02 22:36:16 +0545

    day 7

--- src/clients/cassandra/cassandra_db.go
+++ src/clients/cassandra/cassandra_db.go
@@ -1,22 +1,21 @@
 package cassandra
 
 import (
-	"fmt"
-
 	"github.com/gocql/gocql"
 )
 
-
+var(
+	cluster *gocql.ClusterConfig
+)
  
 func init(){
 	//Connect to cassandra cluster:
-	cluster := gocql.NewCluster("127.0.0.1")
+	cluster = gocql.NewCluster("127.0.0.1")
 	cluster.Keyspace = "oauth"
 	cluster.Consistency = gocql.Quorum
-	session, err := cluster.CreateSession()
-	if err != nil{
-		panic(err)
-	}
-	fmt.Println("cassandra connection successfully created")
-	defer session.Close()
+	
+}
+
+func GetSession()(*gocql.Session, error ){
+	return cluster.CreateSession()
 }
--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"clients/cassandra"
+
 	"github.com/ayush723/oauth-api_bookstore/src/domain/access_token"
 
 	"github.com/ayush723/oauth-api_bookstore/src/utils/errors"
@@ -18,5 +20,10 @@ type dbRepository struct {
 }
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	//TODO: implement get access token from cassandra db
+	_, err := cassandra.GetSession()
+	if err != nil{
+		panic(err)
+	}
 	return nil, errors.NewInternalServerError("database connection not implemented yet")
 }
